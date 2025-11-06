ebb850089073719d48e5ff94f656d2b7f3f50b8f
ManduIon <manduioncostinel@gmail.com>
2025-11-06 17:32:24 +0200

    Modificari :)

--- Pandas&Viz.py
+++ Pandas&Viz.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+import seaborn as sns
 
 #citim csv.urile
 df_event = pd.read_csv('events.csv')
@@ -33,4 +36,41 @@ print(df_items.duplicated().sum()) #verificam cate dublicate avem
 df_items = df_items.drop_duplicates() #Stergem  duplicate
 
 
+#Statistici:
+q1 = df_order['amount'].quantile(0.25)
+q3 = df_order['amount'].quantile(0.75)
+IQR = q3 -q1
 
+Statistici = {
+    'Count': df_order['amount'].count(),
+    'Mean': df_order['amount'].mean(),
+    'Median': df_order['amount'].median(),
+    'P95': np.percentile(df_order['amount'] , 95),
+    'IQR': IQR
+}
+
+
+#Histograma + boxplot pentru 'amount'.
+
+
+status = df_order.status.value_counts()
+print(status)
+df_order_real = df_order[~df_order['status'].isin(['cancelled', 'refunded'])]
+#print(df_order_real['status'].value_counts())
+
+plt.figure(figsize=(12,5))
+#histograma
+plt.subplot(1,2,1)
+sns.histplot(df_order_real['amount'], bins=30, kde=True)
+plt.title('Distributia valorilor amount')
+plt.xlabel('Amount')
+plt.ylabel('Frecventa')
+#boxplot
+plt.subplot(1,2,2)
+sns.boxplot(x = df_order_real['amount'])
+plt.title('Boxplot pentru amount')
+plt.tight_layout()
+plt.show()
+"""
+Explicati---
+ """
